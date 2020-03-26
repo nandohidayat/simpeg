7f20ddd263ddcb5efbde91e3e8dadbb4d8dd7e3b
nandohidayat <mnandohidayat@gmail.com>
2020-03-26 20:34:36 +0700

    setbaseurl is a function u idiot

--- plugins/guard.js
+++ plugins/guard.js
@@ -2,10 +2,10 @@ export default function({ $axios, redirect }) {
   $axios.onRequest((config) => {
     if (!process.server) {
       if (window.location.hostname.split('.')[2] === '0') {
-        $axios.setBaseUrl = 'http://192.168.0.106:8000/api/'
+        $axios.setBaseUrl('http://192.168.0.106:8000/api/')
       }
       if (window.location.hostname.split('.')[2] === '1') {
-        $axios.setBaseUrl = 'http://192.168.1.7:8000/api/'
+        $axios.setBaseUrl('http://192.168.1.7:8000/api/')
       }
     }
   })
