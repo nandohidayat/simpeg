0ab6c7a5cca0c9a3f39ffe5a6ab2109adf0fa0d4
nandohidayat <mnandohidayat@gmail.com>
2020-03-28 18:13:58 +0700

    Revert "lupa ngeganti setbaseurl ke ip 106"
    This reverts commit 2e9cff410481f3bd34b83519b5463058b4f4f388.

--- plugins/token.js
+++ plugins/token.js
@@ -4,10 +4,10 @@ export default ({ $axios, store }) => {
   }
 
   if (window.location.hostname.split('.')[2] === '0') {
-    $axios.setBaseURL('http://192.168.0.106:8000/api/')
+    $axios.setBaseURL('http://192.168.0.216:8000/api/')
   }
   if (window.location.hostname.split('.')[2] === '1') {
-    $axios.setBaseURL('http://192.168.1.7:8000/api/')
+    $axios.setBaseURL('http://192.168.1.216:8000/api/')
   }
 
   $axios.setToken(store.state.user.token, 'Bearer')
