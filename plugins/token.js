b06c780f0faff4baaddee496d0c870d7fb455ae6
nandohidayat <mnandohidayat@gmail.com>
2020-03-28 16:18:55 +0700

    fix to change base and place it on token

--- plugins/token.js
+++ plugins/token.js
@@ -3,5 +3,12 @@ export default ({ $axios, store }) => {
     return
   }
 
+  if (window.location.hostname.split('.')[2] === '0') {
+    $axios.setBaseURL('http://192.168.0.216:8000/api/')
+  }
+  if (window.location.hostname.split('.')[2] === '1') {
+    $axios.setBaseURL('http://192.168.1.216:8000/api/')
+  }
+
   $axios.setToken(store.state.user.token, 'Bearer')
 }
