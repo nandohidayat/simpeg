719b130f83d6c03ca7473d0e7b641b4e3d150eda
nandohidayat <mnandohidayat@gmail.com>
2020-05-13 01:09:08 +0700

    create scheduleaccess store

---
+++ store/scheduleaccess.js
@@ -0,0 +1,44 @@
+export const namespaced = true
+
+export const state = () => ({
+  schedules: []
+})
+
+export const mutations = {
+  SET_SCHEDULES(state, schedules) {
+    state.schedules = schedules
+  },
+  ADD_SCHEDULE(state, schedule) {
+    state.schedules.push(schedule)
+  },
+  EDT_SCHEDULE(state, schedule) {
+    state.schedules = state.schedules.map((s) =>
+      s.id_schedule_access !== schedule.id_schedule_access ? s : schedule
+    )
+  },
+  DEL_SCHEDULE(state, id) {
+    state.schedules = state.schedules.filter((b) => b.id_schedule_access !== id)
+  }
+}
+
+export const actions = {
+  async fetchSchedules({ commit }) {
+    const res = await this.$api.scheduleAssessor.index()
+    commit('SET_SCHEDULES', res.data)
+  },
+  async createSchedule({ commit }, schedule) {
+    const res = await this.$api.scheduleAssessor.create(schedule)
+    commit('ADD_SCHEDULE', res.data)
+  },
+  async updateSchedule({ commit }, schedule) {
+    const res = await this.$api.scheduleAssessor.update(
+      schedule.id_schedule_assessor,
+      schedule
+    )
+    commit('EDT_SCHEDULE', res.data)
+  },
+  async deleteSchedule({ commit }, schedule) {
+    await this.$api.scheduleAssessor.delete(schedule.id_schedule_assessor)
+    commit('DEL_SCHEDULE', schedule.id_schedule_assessor)
+  }
+}
