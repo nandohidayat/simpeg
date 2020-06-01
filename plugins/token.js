de4518ec7485dab92fa58fe181d07941c0af1e09
nandohidayat <mnandohidayat@gmail.com>
2020-06-01 22:29:08 +0700

    penyesuai an url ke public

--- plugins/token.js
+++ plugins/token.js
@@ -4,10 +4,12 @@ export default ({ $axios, store }) => {
   }
 
   if (window.location.hostname.split('.')[2] === '0') {
-    $axios.setBaseURL('http://192.168.0.106:8000/api/')
+    $axios.setBaseURL(
+      'http://192.168.0.106/php74/sp360/SP360-server/public/api/'
+    )
   }
   if (window.location.hostname.split('.')[2] === '1') {
-    $axios.setBaseURL('http://192.168.1.7:8000/api/')
+    $axios.setBaseURL('http://192.168.1.7/php74/sp360/SP360-server/public/api/')
   }
 
   $axios.setToken(store.state.user.token, 'Bearer')
