28f39cd24fd69270afe1e81016d7951526743b0b
nandohidayat <mnandohidayat@gmail.com>
2020-06-27 17:14:27 +0700

    penyesuaian url

--- plugins/baseurl.js
+++ plugins/baseurl.js
@@ -2,8 +2,11 @@ export default ({ $axios }) => {
   if (process.client) {
     const protocol = window.location.protocol
     const hostname = window.location.hostname
-    const port = 8000
-    const url = `${protocol}//${hostname}:${port}/api/`
+    let port
+    if (hostname.split('.')[0] === '117') {
+      port = ':8957'
+    }
+    const url = `${protocol}//${hostname}${port}/php74/simpeg/simpeg_backend/public/api/`
     $axios.defaults.baseURL = url
   }
 }
