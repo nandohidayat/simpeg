4bf5c38129dde8c47bfc2f7c4641e271af59b481
nandohidayat <mnandohidayat@gmail.com>
2020-08-26 21:25:54 +0700

    patch: redesign data jadwal (on progress)

--- components/shift/shift-card.vue
+++ components/shift/shift-card.vue
@@ -1,29 +1,60 @@
 <template>
-  <base-table
-    :header="header"
-    :title="title"
-    :store="store"
-    :state="state"
-    :action="action"
-    :data.sync="data"
-    @reset="reset()"
+  <v-data-table
+    v-model="selShift"
+    :headers="header"
+    :items="shift.shifts"
+    item-key="id_shift"
+    multi-sort
+    show-select
   >
-    <shift-form :data="data"></shift-form>
-  </base-table>
+    <template #top>
+      <v-toolbar flat class="my-2">
+        <v-autocomplete
+          v-model="selDept"
+          class="mr-4"
+          label="Departemen"
+          :items="departemen.departemens"
+          :item-value="(o) => o.id_dept"
+          :item-text="(o) => o.nm_dept"
+          hide-details
+          outlined
+          dense
+          clearable
+          @change="getSelected()"
+        >
+        </v-autocomplete>
+        <v-btn color="teal" dark depressed class="mr-4"
+          ><v-icon left>mdi-content-save</v-icon>Simpan Shift</v-btn
+        >
+        <v-btn color="teal" dark depressed
+          ><v-icon left>mdi-plus</v-icon>Tambah Shift</v-btn
+        >
+      </v-toolbar>
+    </template>
+    <template #item.color="{item}">
+      <v-chip :color="item.color || 'white'" dark>{{ item.color }}</v-chip>
+    </template>
+    <template #item.action>
+      <v-icon small>
+        mdi-delete
+      </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-import baseTable from '@/components/base/base-table'
-import shiftForm from '@/components/shift/shift-form'
+import { mapState } from 'vuex'
+
+// import baseTable from '@/components/base/base-table'
+// import shiftForm from '@/components/shift/shift-form'
 
 export default {
   components: {
-    'base-table': baseTable,
-    'shift-form': shiftForm,
+    // 'base-table': baseTable,
+    // 'shift-form': shiftForm,
   },
   data() {
     return {
-      title: 'Shift',
       header: [
         {
           text: 'Mulai',
@@ -45,12 +76,21 @@ export default {
           text: 'Ket',
           value: 'keterangan',
         },
+        {
+          text: 'Action',
+          value: 'action',
+        },
       ],
       store: 'shift',
       state: 'shifts',
       action: 'Shift',
       data: this.newData(),
+      selDept: undefined,
+      selShift: [],
     }
+  },
+  computed: {
+    ...mapState(['shift', 'departemen']),
   },
   methods: {
     reset() {
@@ -63,6 +103,18 @@ export default {
         kode: undefined,
         color: undefined,
         keterangan: undefined,
+      }
+    },
+    async getSelected() {
+      if (this.selDept === undefined) {
+        this.selShift = []
+      } else {
+        try {
+          await this.$store.dispatch(`shift/fetchDepartemen`, this.selDept)
+          this.selShift = this.shift.departemen
+        } catch (err) {
+          this.$alert('error', err)
+        }
       }
     },
   },
