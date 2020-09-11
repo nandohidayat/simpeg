8b9e64fa60152b3e6cb924ad7967ea95254db7f0
nandohidayat <mnandohidayat@gmail.com>
2020-09-11 16:39:08 +0700

    fix pendapatan pegawai

---
+++ components/pendapatan/pendapatan-send.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-card outlined>
+      <v-card-text>
+        <v-row>
+          <v-col cols="3">
+            <v-autocomplete outlined dense hide-details></v-autocomplete>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field outlined dense hide-details></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-autocomplete outlined dense hide-details></v-autocomplete>
+          </v-col>
+          <v-col cols="3">
+            <v-row no-gutters>
+              <v-col cols="8" class="pr-2">
+                <v-btn block color="teal" dark
+                  ><v-icon left>mdi-email-send</v-icon> kirim email</v-btn
+                >
+              </v-col>
+              <v-col cols="4" class="pl-2">
+                <v-btn block color="teal" dark
+                  ><v-icon>mdi-refresh</v-icon></v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/pendapatan.vue
+++ pages/pendapatan.vue
@@ -3,6 +3,7 @@
     <v-tabs v-model="tab" color="teal" grow>
       <v-tab>Daftar Pendapatan</v-tab>
       <v-tab>Download / Upload Template</v-tab>
+      <v-tab>Kirim Email Pendapatan</v-tab>
       <v-tab>Manage Template</v-tab>
     </v-tabs>
     <v-card-text>
@@ -12,6 +13,9 @@
         </v-tab-item>
         <v-tab-item>
           <download-template></download-template>
+        </v-tab-item>
+        <v-tab-item>
+          <pendapatan-send></pendapatan-send>
         </v-tab-item>
         <v-tab-item>
           <manage-template></manage-template>
@@ -25,12 +29,14 @@
 import DownloadTemplate from '@/components/pendapatan/download-template'
 import ManageTemplate from '@/components/pendapatan/profil/manage-template'
 import PendapatanList from '@/components/pendapatan/pendapatan-list'
+import PendapatanSend from '@/components/pendapatan/pendapatan-send'
 
 export default {
   components: {
     DownloadTemplate,
     ManageTemplate,
     PendapatanList,
+    PendapatanSend,
   },
   async fetch({ store }) {
     await store.dispatch('pendapatanprofil/fetchProfils', { select: 1 })
